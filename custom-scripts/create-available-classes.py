75c8600dcba67a6179560a0ebcc4694ef4cac020
lucio-cornejo <luciocornejo11@gmailcom>
2023-08-15 00:10:26 -0500

    get available classes

--- custom-scripts/create-available-classes.py
+++ custom-scripts/create-available-classes.py
@@ -1,20 +1,22 @@
-import os
+from os import listdir
 
 # Retrieve classes notes
-apuntesPath = "./_apuntes/"
-courses = os.listdir(apuntesPath)
+coursesPath = "./courses/"
+coursesPaths = listdir(coursesPath)
+coursesPaths
 
 # Create JSON file for non empty classes
 final_json = {}
-for course in courses:
-  final_json[course] = []
+for course in coursesPaths:
+  # Assume the /apuntes/ folder only 
+  # contains pdfs, and those are of the 
+  # form "clase-number.pdf" .
+  final_json[course] = listdir(coursesPath + course + "/apuntes")
 
-  classes = os.listdir(apuntesPath + course)
-  for classText in classes:
-    # Retrieve non empty course class 
-    course_class = apuntesPath + course + "/" + classText
-    if os.listdir(course_class):
-      final_json[course].append(classText)
+  if final_json[course]:
+    # Retrieve classes' numbers
+    temp = final_json[course]
+    final_json[course] = [pdfName[6:8] for pdfName in temp]
 
 # Save dictionary as JSON
 import json
